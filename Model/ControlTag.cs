44305538cf36fa59a310bbe3513d26cd90ca3f1c
zhudahe@outlook.com <zhudahe@outlook.com>
2017-03-30 15:25:17 +0800

    修改文件发送和数据库存储位置

---
+++ Model/ControlTag.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace Model
+{
+    public class ControlTag
+    {
+        private string _md5;
+        private string _fileName;
+        private IPEndPoint _remoteIP;
+
+        public ControlTag(
+            string md5,
+            string fileName,
+            IPEndPoint remoteIP)
+        {
+            _md5 = md5;
+            _fileName = fileName;
+            _remoteIP = remoteIP;
+        }
+
+        public string MD5
+        {
+            get { return _md5; }
+            set { _md5 = value; }
+        }
+
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+        public IPEndPoint RemoteIP
+        {
+            get { return _remoteIP; }
+            set { _remoteIP = value; }
+        }
+    }
+}
---
+++ UDPIMClient/SendFileForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace UDPIMClient
+{
+    partial class SendFileForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbLocalPort = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbRemotePort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbRemoteIP = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fileTansfersContainer = new CSharpWin.FileTansfersContainer();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "本机监听端口";
+            // 
+            // tbLocalPort
+            // 
+            this.tbLocalPort.Location = new System.Drawing.Point(95, 6);
+            this.tbLocalPort.Name = "tbLocalPort";
+            this.tbLocalPort.Size = new System.Drawing.Size(100, 21);
+            this.tbLocalPort.TabIndex = 2;
+            this.tbLocalPort.Text = "10002";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "开始监听";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbRemotePort
+            // 
+            this.tbRemotePort.Location = new System.Drawing.Point(297, 37);
+            this.tbRemotePort.Name = "tbRemotePort";
+            this.tbRemotePort.Size = new System.Drawing.Size(100, 21);
+            this.tbRemotePort.TabIndex = 13;
+            this.tbRemotePort.Text = "10002";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "接收方监听端口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "接收方IP";
+            // 
+            // tbRemoteIP
+            // 
+            this.tbRemoteIP.Location = new System.Drawing.Point(96, 37);
+            this.tbRemoteIP.Name = "tbRemoteIP";
+            this.tbRemoteIP.Size = new System.Drawing.Size(100, 21);
+            this.tbRemoteIP.TabIndex = 10;
+            this.tbRemoteIP.Text = "127.0.0.1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.tbRemotePort);
+            this.panel1.Controls.Add(this.tbLocalPort);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tbRemoteIP);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 217);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(489, 100);
+            this.panel1.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Controls.Add(this.fileTansfersContainer);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(489, 217);
+            this.panel2.TabIndex = 16;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(240, 217);
+            this.listBox1.TabIndex = 3;
+            // 
+            // fileTansfersContainer
+            // 
+            this.fileTansfersContainer.AutoScroll = true;
+            this.fileTansfersContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(250)))), ((int)(((byte)(249)))));
+            this.fileTansfersContainer.Dock = System.Windows.Forms.DockStyle.Right;
+            this.fileTansfersContainer.Location = new System.Drawing.Point(240, 0);
+            this.fileTansfersContainer.Name = "fileTansfersContainer";
+            this.fileTansfersContainer.Size = new System.Drawing.Size(249, 217);
+            this.fileTansfersContainer.TabIndex = 0;
+            // 
+            // SendFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 317);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "SendFileForm";
+            this.Text = "发送文件";
+            this.Load += new System.EventHandler(this.SendFileForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbLocalPort;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbRemotePort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbRemoteIP;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private CSharpWin.FileTansfersContainer fileTansfersContainer;
+    }
+
+}
